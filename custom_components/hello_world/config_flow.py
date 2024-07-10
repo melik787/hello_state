41337931bf46f92ba38d135066567c7b8a7d15ab
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-10 13:09:16 +0200

    Changed config flow

--- custom_components/hello_world/config_flow.py
+++ custom_components/hello_world/config_flow.py
@@ -9,7 +9,7 @@ class HelloStateFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Hello, state!"""
 
     VERSION = 1
-    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+    MINOR_VERSION = 1
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
