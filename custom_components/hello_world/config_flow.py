0d9f52815818a6dbaa0a3a7a629136f8f38a5fac
Kerim Melikoglu <Kerim.Melikoglu@my-pv.com>
2024-07-16 15:27:03 +0200

    Updated config_flow.py

--- custom_components/hello_world/config_flow.py
+++ custom_components/hello_world/config_flow.py
@@ -13,17 +13,27 @@ class HelloStateFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
 
         if user_input is not None:
-            host = user_input[CONF_HOST]
-            return self.async_create_entry(
-                title=f"Hello {host}",
-                data={CONF_HOST: host},
-            )
+            if user_input["button"] is "button_1":
+                return await self.async_step_host()
+            elif user_input["button"] is "button_2":
+                return await self.async_step_host()
 
-        return self.async_show_form(
+        return self.async_show_menu(
             step_id="user",
+            menu_options=[
+                {"button": "button_1", "description": "IP bekannt"},
+                {"button": "button_2", "description": "IP unbekannt"},
+            ],
+        )
+    
+    async def async_step_host(self, user_input=None):
+        if user_input is not None:
+            host = user_input[CONF_HOST]
+            return self.async_create_entry(title="Hello world!", data={CONF_HOST: host})
+        
+        return self.async_show_form(
+            step_id="host",
             data_schema=vol.Schema({vol.Required(CONF_HOST): str}),
-            errors=errors,
         )
